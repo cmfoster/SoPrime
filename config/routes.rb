3926683b8b4c530049449a79a5057e90735faae3
cmfoster <curtismfoster@gmail.com>
2012-06-25 04:33:47 -0500

    added images and more src

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Soprime::Application.routes.draw do
   devise_for :users
   root :to => "home#index"
   match "weddings" => "home#weddings", :as => :weddings
+  match "clubs" => "home#clubs", :as => :clubs
   
   resource :request_forms, :only => [:create, :update, :new]
 end
