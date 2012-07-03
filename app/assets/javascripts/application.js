eaf1ee2e20cd7987e5473b7f4bc6bc08073aad91
cmfoster <curtismfoster@gmail.com>
2012-07-02 22:26:29 -0500

    added gallery viewer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require jquery.timers-1.2
+//= require jquery.easing.1.3.js
+//= require jquery.galleryview-3.0-dev
 //= require_tree .
